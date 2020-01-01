d0e402d41919be4dae41374d8fc69d0c96f4d3a5
blinoff82 <blinov.in@gmail.com>
2020-01-01 10:32:34 +0300

    Build Square icon overlay
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: Ic7621cf5a144ff31a7651867e7ddc52f5642484e

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -61,6 +61,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconShapeRoundedRectOverlay \
+	IconShapeSquareOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
 	NavigationBarMode3ButtonOverlay \
