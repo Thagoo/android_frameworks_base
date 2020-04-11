f0f66e537b6f9395f4f02cc75e1ac3125ab45958
Anushek Prasal <anushekprasal@gmail.com>
2020-04-12 02:20:51 +0530

    base: Cleanup hide navbar pill overlays

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -71,10 +71,6 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlayNarrowBack \
 	NavigationBarModeGesturalOverlayWideBack \
 	NavigationBarModeGesturalOverlayExtraWideBack \
-	NavigationBarModeGesturalOverlayNoPill \
-	NavigationBarModeGesturalOverlayNarrowBackNoPill \
-	NavigationBarModeGesturalOverlayWideBackNoPill \
-	NavigationBarModeGesturalOverlayExtraWideBackNoPill \
 	PrimaryColorBlackOverlay \
 	PrimaryColorDarkBlueOverlay
 
