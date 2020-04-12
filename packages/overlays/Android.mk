833cb0720ab3c92e115027b4cda9383b8ad1120f
officialyash007 <officialyash007@gmail.com>
2020-04-12 13:38:10 +0000

    Add Reemkufi font [2/2]
      * picked from Project-Xtended
      * thanks @sai4041412
    
    Signed-off-by: officialyash007 <officialyash007@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -55,6 +55,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontLGSmartGothicSourceOverlay \
 	FontNotoSerifSourceOverlay \
 	FontOneplusSlateSource \
+	FontReemKufiSourceOverlay \
 	FontRosemarySourceOverlay \
 	FontRubikRubikOverlay \
 	FontSamsungOneSourceOverlay \
